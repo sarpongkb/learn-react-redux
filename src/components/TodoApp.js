912c9f92cdcdfeed73bd574e443f5fb741307016
Boadu Mensah Sarpong <boadu@bamsas.com>
2017-09-26 11:18:04 -0700

    Ready for todo app

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TodoApp.css';
+
+export class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Ready for todo app
+        </p>
+      </div>
+    );
+  }
+}
