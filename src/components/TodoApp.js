1b30602eaa0a0ac84d3ad9190f0913a31219b17e
sarpongkb <sarpongkb@gmail.com>
2017-09-26 21:39:06 -0700

    prefer functional components

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,48 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import "./TodoApp.css";
 
 import { addTodo, toggleTodo, deleteTodo, setVisibilityFilter, setInputText } from "../reduxStore";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.onAddTodo = this.onAddTodo.bind(this);
-    this.updateTextInput = this.updateTextInput.bind(this);
-  }
-
-  onAddTodo() {
-    const text = this.props.inputText.trim();
+const App = props => {
+  const onAddTodo = () => {
+    const text = props.inputText.trim();
     if (text) {
-      this.props.addTodo(text);
-      this.props.setInputText("");
+      props.addTodo(text);
+      props.setInputText("");
     }
   }
 
-  updateTextInput(e) {
-    this.props.setInputText(e.target.value);
-  }
+  const updateTextInput = e => props.setInputText(e.target.value);
 
-  render() {
-    return (
-      <div>
-        <input type="text" onChange={this.updateTextInput} value={this.props.inputText} />
-        <button onClick={this.onAddTodo}>Add Todo</button>
-        <ul>
-          {
-            this.props.todos.map(todo => (
-              <li key={todo.id}>
-                { todo.text }
-                <button onClick={this.props.deleteTodo.bind(null, todo.id)}>X</button>
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    );
-  }
-
-}
+  return (
+    <div>
+      <input type="text" onChange={updateTextInput} value={props.inputText} />
+      <button onClick={onAddTodo}>Add Todo</button>
+      <ul>{
+        props.todos.map(todo => (
+          <li key={todo.id}>
+            { todo.text }
+            <button onClick={props.deleteTodo.bind(null, todo.id)}>X</button>
+          </li>
+        ))
+      }</ul>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ todos, visibilityFilter, inputText }) => ({
   todos,
